9eb667fa729a8be37b0d83a74eb9d1aac83cca12
JeremyMqy <jeremyqiyang@gmail.com>
2024-04-16 16:07:06 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+todos = []
+
+while True:
+    user_action = input("type add or show")
+    match user_action:
+        case 'add':
+            todo = input("enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
